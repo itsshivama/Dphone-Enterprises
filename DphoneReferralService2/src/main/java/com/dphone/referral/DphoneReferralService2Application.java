ca11a6f5ae5eee2c5eb639294adf9d1fa128d1f9
Sankalp-cyber <sankalpgupta811@gmail.com>
2023-02-20 02:49:54 +0530

    commit

--- DphoneReferralService2/src/main/java/com/dphone/referral/DphoneReferralService2Application.java
+++ DphoneReferralService2/src/main/java/com/dphone/referral/DphoneReferralService2Application.java
@@ -8,6 +8,7 @@ public class DphoneReferralService2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DphoneReferralService2Application.class, args);
+		System.out.println("Referral Application Started");
 	}
 
 }
